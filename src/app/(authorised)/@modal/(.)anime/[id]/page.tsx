b7c3a00821726cad0ef22e1c0c33af5a4b244e34
Jake <jake@hwll.me>
2025-06-16 22:37:57 +1000

    feat: convert route intercept to modal

--- src/app/(authorised)/@modal/(.)anime/[id]/page.tsx
+++ src/app/(authorised)/@modal/(.)anime/[id]/page.tsx
@@ -1,8 +1,43 @@
+"use client";
+
+import { Button, Dialog, Portal } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+
 export default function Page({
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   params: _,
 }: {
   params: Promise<{ id: string }>;
 }) {
-  return <div>TODO</div>;
+  const router = useRouter();
+
+  const onOpenChange = () => {
+    router.back();
+  };
+
+  return (
+    <Dialog.Root open={true} onOpenChange={onOpenChange}>
+      <Portal>
+        <Dialog.Backdrop />
+        <Dialog.Positioner>
+          <Dialog.Content>
+            <Dialog.Header>
+              <Dialog.Title>Dialog Title</Dialog.Title>
+            </Dialog.Header>
+            <Dialog.Body>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </p>
+            </Dialog.Body>
+            <Dialog.Footer>
+              <Dialog.ActionTrigger asChild>
+                <Button variant="outline">Close</Button>
+              </Dialog.ActionTrigger>
+            </Dialog.Footer>
+          </Dialog.Content>
+        </Dialog.Positioner>
+      </Portal>
+    </Dialog.Root>
+  );
 }

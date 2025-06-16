2ad299e2c9bbff3843a2e0c3c3f246e41237ced0
Jake <jake@hwll.me>
2025-06-16 21:47:48 +1000

    feat: instantiate a basic `/unauthorised` content page

--- src/app/(auth)/unauthorised/page.tsx
+++ src/app/(auth)/unauthorised/page.tsx
@@ -1,3 +1,35 @@
+import { Button, Card, Container, Field, Input, Stack } from "@chakra-ui/react";
+
 export default function Unauthorised() {
-  return <h1>Foo</h1>;
+  return (
+    <Container width={"md"}>
+      <Card.Root>
+        <Card.Header>
+          <Card.Title>Unauthorised</Card.Title>
+          <Card.Description>
+            {
+              "You're unable to access this content without providing some details first."
+            }
+          </Card.Description>
+        </Card.Header>
+        <Card.Body gap="2">
+          <Stack gap="4" w="full">
+            <Field.Root>
+              <Field.Label>Username</Field.Label>
+              <Input />
+            </Field.Root>
+            <Field.Root>
+              <Field.Label>Job Title</Field.Label>
+              <Input />
+            </Field.Root>
+          </Stack>
+        </Card.Body>
+        <Card.Footer>
+          <Button variant="subtle" width={"full"}>
+            Sign In
+          </Button>
+        </Card.Footer>
+      </Card.Root>
+    </Container>
+  );
 }

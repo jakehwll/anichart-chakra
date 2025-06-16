a81cfed21f7875e1001de79be5b64e1fdc987fa1
Jake <jake@wnxr-m1.local>
2025-06-16 20:56:52 +1000

    chore: init

---
+++ README.md
@@ -0,0 +1,12 @@
+# `anichart`
+
+### Introduction
+
+This is a `nextjs` project containing and implementation of `anichart.net` written using ChakraUI.
+
+### Technologies
+
+* [React](https://react.dev/) - Frontend
+* [Next.js](https://nextjs.org/) - Server-side generation and API.
+* [ChakraUI](https://chakra-ui.com/)
+* [Bun](https://bun.sh/)
---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "AniChart",
+  description: "Keep up with the buzz and find out what everyone's reading today.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}

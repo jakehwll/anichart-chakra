497ea792b06302f9950878374ef69839fccc642c
Jake <jake@hwll.me>
2025-06-23 05:00:22 +1000

    feat: `<PaginationCount />`

--- src/components/Series.tsx
+++ src/components/Series.tsx
@@ -1,15 +1,24 @@
 "use client";
 
-import { Grid } from "@chakra-ui/react";
+import { Flex, Grid, IconButton } from "@chakra-ui/react";
 import SeriesSkeleton from "./SeriesSkeleton";
 import SeriesCard from "./SeriesCard";
 import { useQuery } from "@apollo/client";
 import { gql } from "@/__generated__";
 import { MediaSeason } from "@/__generated__/graphql";
+import { Pagination } from "@chakra-ui/react";
+import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
+import { useState } from "react";
+import { PaginationPage } from "./PaginationCount";
+
+const PER_PAGE = 12;
 
 const GET_SERIES_LIST = gql(`
-  query GetSeries($season: MediaSeason, $seasonYear: Int) {
-    Page(page: 1, perPage: 12) {
+  query GetSeries($season: MediaSeason, $seasonYear: Int, $page: Int, $perPage: Int) {
+    Page(page: $page, perPage: $perPage) {
+      pageInfo {
+        total
+      }
       media(
         season: $season
         seasonYear: $seasonYear
@@ -45,8 +54,10 @@ const Series = ({
   season: MediaSeason;
   seasonYear: number;
 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
   const { data, error } = useQuery(GET_SERIES_LIST, {
-    variables: { season, seasonYear },
+    variables: { season, seasonYear, page: currentPage, perPage: PER_PAGE },
   });
 
   if (error) {
@@ -54,32 +65,57 @@ const Series = ({
   }
 
   return (
-    <Grid
-      width={"full"}
-      templateColumns="repeat(1, 1fr)"
-      md={{
-        gridTemplateColumns: "repeat(2, 1fr)",
-      }}
-      xl={{
-        gridTemplateColumns: "repeat(3, 1fr)",
-      }}
-      gap={4}
-    >
-      {!data && (
-        <>
-          {new Array(9).fill(null).map((_, i) => (
-            <SeriesSkeleton key={i} />
-          ))}
-        </>
-      )}
-      {data && (
-        <>
-          {data.Page?.media?.map((media) =>
-            media !== null ? <SeriesCard key={media.id} {...media} /> : media
-          )}
-        </>
-      )}
-    </Grid>
+    <Flex flexDir={"column"} gap={2} width={"full"}>
+      <Grid
+        width={"full"}
+        templateColumns="repeat(1, 1fr)"
+        md={{
+          gridTemplateColumns: "repeat(2, 1fr)",
+        }}
+        xl={{
+          gridTemplateColumns: "repeat(3, 1fr)",
+        }}
+        gap={4}
+      >
+        {!data && (
+          <>
+            {new Array(9).fill(null).map((_, i) => (
+              <SeriesSkeleton key={i} />
+            ))}
+          </>
+        )}
+        {data && (
+          <>
+            {data.Page?.media?.map((media) =>
+              media !== null ? <SeriesCard key={media.id} {...media} /> : media
+            )}
+          </>
+        )}
+      </Grid>
+      <Pagination.Root
+        defaultPage={1}
+        pageSize={PER_PAGE}
+        count={data?.Page?.pageInfo?.total ?? 0}
+        onPageChange={({ page }) => setCurrentPage(page)}
+        width={"full"}
+      >
+        <Flex alignItems={"center"} gap={4}>
+          <Pagination.PrevTrigger asChild>
+            <IconButton variant={"ghost"}>
+              <LuChevronLeft />
+            </IconButton>
+          </Pagination.PrevTrigger>
+
+          <PaginationPage />
+
+          <Pagination.NextTrigger asChild>
+            <IconButton variant={"ghost"}>
+              <LuChevronRight />
+            </IconButton>
+          </Pagination.NextTrigger>
+        </Flex>
+      </Pagination.Root>
+    </Flex>
   );
 };
 
